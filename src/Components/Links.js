fdd6a47f3b7173009f3ab6ac542fd45fe0896d80
vickel <vickelleung@gmail.com>
2020-09-25 22:44:57 -0400

    38: updated navigation UI

--- src/Components/Links.js
+++ src/Components/Links.js
@@ -2,13 +2,24 @@ import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import { COLORS } from "../Styles/Color";
+import Logo from "../assets/Logo/logo.png";
 
 const Links = () => {
   return (
     <Wrapper>
-      <LinkItem to="/services">services</LinkItem>
-      <LinkItem to="/shop">shops</LinkItem>
-      <LinkItem to="/auth">signin</LinkItem>
+      <LeftSideWrapper>
+        <LinkItem to="/">
+          <img style={{ height: "5vh", width: "5vw" }} src={Logo}></img>
+        </LinkItem>
+      </LeftSideWrapper>
+
+      <MiddleWrapper>
+        <LinkItem to="/services">services</LinkItem>
+        <LinkItem to="/shop">shops</LinkItem>
+      </MiddleWrapper>
+      <RightSideWrapper>
+        <LinkItem to="/auth">signin</LinkItem>
+      </RightSideWrapper>
     </Wrapper>
   );
 };
@@ -17,7 +28,7 @@ export default Links;
 //MOBILE-> flex-direction: row-reverse;
 
 const LinkItem = styled(Link)`
-  margin: 3%;
+  padding: 0 5%;
   color: white;
   text-decoration: none;
   outline: none;
@@ -28,11 +39,20 @@ const LinkItem = styled(Link)`
 `;
 
 const Wrapper = styled.div`
+  padding: 1%;
   display: flex;
   flex-direction: rows;
-
-  justify-content: center;
-
+  justify-content: space-between;
   border: 1px solid black;
   background-color: #242a47;
+`;
+
+const LeftSideWrapper = styled.div`
+  margin-left: 2%;
+`;
+
+const MiddleWrapper = styled.div``;
+
+const RightSideWrapper = styled.div`
+  margin-right: 2%;
 `;
