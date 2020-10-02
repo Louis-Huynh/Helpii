14e2f17f13ef59b609da4f54a182a77c92ae9756
vickel <vickelleung@gmail.com>
2020-10-01 22:33:33 -0400

    44: fixed chatbox message

--- src/Section/Chat.js
+++ src/Section/Chat.js
@@ -1,54 +1,79 @@
-import React, { useState, useEffect } from "react";
+import React, { Component, useState, useEffect } from "react";
 import styled from "styled-components";
 
-const Chat = () => {
-  const URL = "ws://localhost:8080/";
-  const ws = new WebSocket(URL);
+const URL = "ws://localhost:8080/";
 
-  const [input, setInput] = useState(null);
-  const [chatMessage, setChatMessage] = useState([]);
+class Chat extends Component {
+  state = {
+    messages: [],
+    input: "",
+  };
 
-  const sendBackend = (e) => {
+  ws = new WebSocket(URL);
+
+  sendBackend = (e) => {
     console.log("send");
     e.preventDefault();
 
+    // console.log(this.state.input);
     const message = {
       type: "chat",
-      payload: input,
+      payload: this.state.input,
     };
 
-    ws.send(JSON.stringify(message));
+    this.ws.send(JSON.stringify(message));
   };
 
-  useEffect(() => {
-    ws.onopen = () => {
+  addMessages = (msg) => {
+    let obj = this.state.messages;
+    obj.push(msg);
+    this.setState({
+      message: obj,
+    });
+  };
+
+  componentDidMount = () => {
+    this.ws.onopen = () => {
       console.log("connected");
     };
 
-    ws.onmessage = (evt) => {
+    this.ws.onmessage = (evt) => {
+      console.log("test");
       // console.log(`[message] Data receive from server: ${evt.data}`);
-      let tempArr = chatMessage;
-      tempArr.push(JSON.parse(evt.data).payload);
-      setChatMessage(tempArr);
+      //console.log(chatMessage);
+      console.log(evt.data);
+      let type = JSON.parse(evt.data).type;
+      let payload = JSON.parse(evt.data).payload;
+
+      if (type == "chat") {
+        this.addMessages(payload);
+      }
     };
 
-    ws.onclose = (evt) => {
+    this.ws.onclose = (evt) => {
       console.log(`Closed session!!`);
     };
-  }, []);
+  };
 
-  return (
-    <div>
-      <Chatbox>
-        {chatMessage.map((x, index) => {
-          return <p key={index}>{x}</p>;
-        })}
-      </Chatbox>
-      <input onChange={(e) => setInput(e.target.value)}></input>
-      <button onClick={sendBackend}>click me</button>
-    </div>
-  );
-};
+  render() {
+    // const [input, setInput] = useState(null);
+    // const [chatMessage, setChatMessage] = useState([]);
+
+    return (
+      <div>
+        <Chatbox>
+          {this.state.messages.map((x, index) => {
+            return <p key={index}>{x}</p>;
+          })}
+        </Chatbox>
+        <input
+          onChange={(e) => this.setState({ input: e.target.value })}
+        ></input>
+        <button onClick={this.sendBackend}>click me</button>
+      </div>
+    );
+  }
+}
 
 export default Chat;
 
